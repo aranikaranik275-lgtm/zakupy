120e7c9cd8af6d3da39a02a1819e7a7cab136940
aranikaranik275-lgtm <aranikaranik275@gmail.com>
2026-03-28 15:01:01 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'zakupy-v5';
+const CACHE = 'zakupy-v6';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
